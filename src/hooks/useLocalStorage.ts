1bdad8287bdb77677542013deef7b03727a271d4
SilentCrouwd <chris.buchholz.lauenau@googlemail.com>
2026-07-10 20:04:57 +0200

    fix: update handleLocalStorage function to accept an array of User objects

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -3,7 +3,7 @@ import type { User } from "./useReducerCrud";
 const LOCAL_STORAGE_KEY = "User";
 
 export function useSetLocalStorage() {
-  function handleLocalStorage(dataObj: User) {
+  function handleLocalStorage(dataObj: User[]) {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(dataObj));
   }
   return { handleLocalStorage };
