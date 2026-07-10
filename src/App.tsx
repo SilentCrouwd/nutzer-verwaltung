88c8a45bb55374632639400d42d6708eb0828547
SilentCrouwd <chris.buchholz.lauenau@googlemail.com>
2026-07-10 20:02:44 +0200

    feat: refactor user management with context and reducer; streamline user creation and editing processes

--- src/App.tsx
+++ src/App.tsx
@@ -4,67 +4,43 @@ import Overview from "./routes/overview/overview";
 import EditView from "./routes/editView/EditView";
 import CreateView from "./routes/createView/CreateView";
 import Root from "./routes/root/Root";
-import { UserContext } from "./hooks/userContext";
-import type { user } from "./hooks/userContext";
-import { useEffect, useReducer, useState } from "react";
-import {
-  useGetLocalStorage,
-  useSetLocalStorage,
-} from "./hooks/useLocalStorage";
-
+import { useState, useReducer, useEffect } from "react";
+import { UserContext } from "./hooks/useContext";
 import { RenderContext } from "./hooks/useRenderContext";
-import { formInputReducer, initialState } from "./hooks/useFormInputReducer";
+import { userReducer, initialUserState } from "./hooks/useReducerCrud";
+import { useSetLocalStorage } from "./hooks/useLocalStorage";
+
 const router = createBrowserRouter(
   [
     {
       path: "/",
       element: <Root />,
       children: [
-        {
-          path: "overview",
-          element: <Overview />,
-        },
-        {
-          path: "create",
-          element: <CreateView />,
-        },
-        {
-          path: "edit/:id",
-          element: <EditView />,
-        },
+        { path: "overview", element: <Overview /> },
+        { path: "create", element: <CreateView /> },
+        { path: "edit/:id", element: <EditView /> },
       ],
     },
   ],
-  {
-    basename: "nutzer-verwaltung",
-  },
+  { basename: "nutzer-verwaltung" },
 );
+
 function App() {
   const [render, setRender] = useState<boolean>(false);
+
+  const [state, dispatch] = useReducer(userReducer, initialUserState);
   const { handleLocalStorage } = useSetLocalStorage();
-  const [userArray, setUserArray] = useState<user[]>([]);
-  const { handleGetLocalStorage } = useGetLocalStorage();
 
   useEffect(() => {
-    const storedUser = handleGetLocalStorage();
-    if (storedUser && storedUser.length > 0) {
-      setUserArray(storedUser);
-    }
-  }, [render]);
+    handleLocalStorage(state.users);
+  }, [state.users, handleLocalStorage]);
 
-  useEffect(() => {
-    handleLocalStorage(userArray);
-  }, [userArray]);
-
-  const addUser = (newUser: user) => {
-    setUserArray((prevUsers) => [...prevUsers, { ...newUser, id: Date.now() }]);
-  };
   return (
-    <RenderContext value={{ render, setRender }}>
-      <UserContext value={{ userArray, addUser }}>
+    <UserContext value={{ state, dispatch }}>
+      <RenderContext value={{ render, setRender }}>
         <RouterProvider router={router} />
-      </UserContext>
-    </RenderContext>
+      </RenderContext>
+    </UserContext>
   );
 }
 
